7ad0e76c43495a91d8debd79aa6ddf67b68b8147
Gabriella Fernandes <141179596+S4t8rn0@users.noreply.github.com>
2023-09-19 12:11:30 -0300

    Imagem.py

--- Imagem.py
+++ Imagem.py
@@ -1,32 +1,39 @@
 import cv2
 import mediapipe as mp
-import matplotlib .pyplot as plt
+import matplotlib.pyplot as plt
+import numpy
 
 webcam = cv2.VideoCapture(0)
-reconhecimento = mp.solutiuons.face_detection
-reconhecedor = reconhecimento.FaceDetection()
+reconhecimento = mp.solutions.face_detection
 desenho=mp.solutions.drawing_utils
-verificador, frame = webcam.read()
-if not verificador:
+reconhecedor = reconhecimento.FaceDetection()
+
+
+while webcam.isOpened():
+    verificador, frame = webcam.read()
     lista_rostos= reconhecedor.process(frame)
 
-if lista_rostos.detection:
-    for rosto in lista_rostos.detection:
-        desenho.draw_detection(frame,rosto)
+    if lista_rostos.detections:
+        for rosto in lista_rostos.detections:
+            desenho.draw_detection(frame, rosto)
 
-    if cv2.waitKey(5) == 27:
-        cv2.imwrite('.png', frame)
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(5) == ord('q'):
+        cv2.imwrite('OK.png', frame)
+    if cv2.waitKey(0)== 27:
         break
 
-cv2.imshow('Rostos', frame)
+
+if not verificador:
+    print('ok')
+    
+
 webcam.release()
 cv2.destroyAllWindows()
 
 #Precisa colocar o endereço da imagem entre parenteses
-img= cv2.imread('', cv2.IMREAD_COLOR)
+img= cv2.imread('OK.png', cv2.IMREAD_COLOR)
 img_rgb= cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 plt.imshow(img)
-
-
-
